f5e5d9ff404ac6102a501c516aa518d72b385977
DESKTOP-KBADI8T\Dell <philip>
2025-11-04 21:22:29 +0000

    First commit

---
+++ src/main/java/com/example/loginMana/LoginmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginMana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginmanagementApplication.class, args);
+	}
+
+}
